c4fa0f952a56c36a52477b4f83acb1be6d5dfef9
Renate Kikkert <renatekikkert@gmail.com>
2014-03-27 13:50:05 +0100

    Add homework assignments for lesson 4.

---
+++ webapp/v1/static/js/app.js
@@ -0,0 +1,33 @@
+(function() {
+  //controller function
+  var controller = {
+    init: function() {
+          var filmHeaders = document.querySelectorAll('.film h3');
+          var filmElements = document.querySelectorAll('.film');
+          for (var i = 0; i < filmHeaders.length; i++) {
+              var filmHeader = filmHeaders[i];
+              console.log(filmHeader.innerHTML);
+          }
+          for (var j = 0; j < filmElements.length; j++) {
+              var filmElement = filmElements[j];
+              Hammer(filmElement).on("tap", function(event) {
+                console.info('TAP EVENT', event);
+                event.target.classList.toggle('active');
+              });
+          }
+    },
+    addFirst: function() {
+          var firstFilm = document.querySelector('.film');
+          firstFilm.classList.add('first');
+    },
+    hideNav:  function() {
+          var navigation = document.querySelector('nav');
+          navigation.classList.add('visually-hidden');
+    }
+  };
+
+  //calling the controller function
+  controller.init();
+  controller.addFirst();
+  controller.hideNav();
+}());
