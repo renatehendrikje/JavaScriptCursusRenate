c75fe5d7c3b4e7d05c614fe99347a552a7e7202e
Renate Kikkert <renatekikkert@gmail.com>
2014-04-02 18:22:19 +0200

    Remove content from HTML and add this with data-binding.

--- webapp/v1/static/js/app.js
+++ webapp/v1/static/js/app.js
@@ -4,13 +4,15 @@
 		init: function() {
 			films.init();
 			router.init();
+			sections.init();
 		}
   	};
 
 	var films = {
 		init: function() {
 			this.printHeaders();
-			this.tapFilmElements();
+			/*this.tapNavLinks();*/
+			/*this.tapFilmElements();*/
 		},
 		printHeaders: function() {
 			var filmHeaders = document.querySelectorAll('.film h3');
@@ -18,7 +20,22 @@
 				var filmHeader = filmHeaders[i];
 				console.log(filmHeader.innerHTML);
 			}
-		},
+		}/*,
+		tapNavLinks: function() {
+			var navFilms = document.querySelectorAll('.nav-films');
+			var navAbout = document.querySelectorAll('.nav-about');
+			var aboutSection = document.querySelectorAll('.about-section');
+			var filmSection = document.querySelectorAll('.film-section');
+			Hammer(navFilms).on("tap", function(event) {
+				aboutSection.classList.add('not-displayed');
+				filmSection.classList.remove('not-displayed');
+			})
+			Hammer(navAbout).on("tap", function(event) {
+				filmSection.classList.add('not-displayed');
+				aboutSection.classList.remove('not-displayed');
+			})
+		}*/
+		/*,
 		tapFilmElements: function() {
 			var filmElements = document.querySelectorAll('.film');
 			for (var j = 0; j < filmElements.length; j++) {
@@ -28,7 +45,8 @@
 					this.classList.toggle('active');
 				});
 			}
-		}/*,
+		}*/
+		/*,
 		addFirst: function() {
 			var firstFilm = document.querySelector('.film');
 			firstFilm.classList.add('first');
@@ -43,10 +61,10 @@
 		init: function() {
 			routie({
 				'/about': function() {
-					console.log('"about" was clicked');
+					/*console.log('"about" was clicked');*/
 				},
 				'/films': function() {
-					console.log('"films" was clicked');
+					/*console.log('"films" was clicked');*/
 				}
 			});
 		}
@@ -54,17 +72,59 @@
 
 	var content = {
 		about: {
-			titel: 'Over deze website',
-			description: ''
+			title: 'Over',
+			description: 'Op deze website laat ik jullie mijn favoriete films zien. Daar zaten jullie vast net op te wachten!'
 		},
-		movies: []
+		movies: [
+			{	title: 'My Little Pony: The Movie',
+				releaseDate: '1986',
+				description: 'Een hartverwarmende film over vriendschap en gekleurde ponys die kunnen praten.',
+				cover: 'my-little-pony.jpg'
+			}, {
+				title: 'Titanic',
+				releaseDate: '1997',
+				description: 'Er was eens een grote boot. Er zaten heel veel mensen op. Toen zonk de boot. Behalve dat ' +
+					'bijna iedereen op de boot dood was, was het ook heel tragisch dat Kate Winslet haar Leo kwijt was.',
+				cover: 'titanic.jpg'
+			}, {
+				title: 'Bananas in Pyjamas',
+				releaseDate: '1992',
+				description: 'Een zeer educatieve film die je kinderen leert dat bananen ook maar mensen zijn.',
+				cover: 'bananas-in-pyjamas.jpg'
+			}, {
+				title: 'Beethoven',
+				releaseDate: '1992',
+				description: 'Sint-Bernards zijn slimmer dan je denkt, dat zie je wel in deze film.',
+				cover: 'beethoven.jpg'
+			}
+		]
 	};
 
+	var sections = {
+		init: function() {
+			this.about();
+			this.movies();
+		},
+		about: function() {
+			Transparency.render(document.getElementById('about'), content.about);
+		},
+		movies: function() {
+			var directives = {
+				cover: {
+					src: function() {
+						return 'static/images/' + this.cover;
+					}
+				}
+			}
+
+			Transparency.render(document.getElementById('movies'), content.movies, directives);
+		}
+	};
 
   //calling the controller function
   controller.init();
 }());
 
-//Het 'about'-object bevat twee properties; 'titel' en 'description'
+
 //De 'movies'-array bevat voor elke film een object met de properties; 'title', 'releaseDate', 'description' en 'cover'
 //stop er wat strings met tekst in
